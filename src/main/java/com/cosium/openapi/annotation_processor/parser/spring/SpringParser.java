e9c0df55f3196824cd8edf3b10c429ab32e44a41
Réda Housni Alaoui <reda.housnialaoui@gmail.com>
2018-10-28 14:44:24 +0100

    Consider @RequestParam

--- src/main/java/com/cosium/openapi/annotation_processor/parser/spring/SpringParser.java
+++ src/main/java/com/cosium/openapi/annotation_processor/parser/spring/SpringParser.java
@@ -13,6 +13,7 @@ import io.swagger.models.Response;
 import io.swagger.models.parameters.BodyParameter;
 import io.swagger.models.parameters.Parameter;
 import io.swagger.models.parameters.PathParameter;
+import io.swagger.models.parameters.QueryParameter;
 import io.swagger.models.properties.ObjectProperty;
 import io.swagger.models.properties.Property;
 import io.swagger.models.properties.StringProperty;
@@ -23,10 +24,23 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.lang.model.element.*;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.Modifier;
+import javax.lang.model.element.VariableElement;
 import javax.lang.model.type.TypeKind;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Stream;
 
 import static java.util.Collections.emptyList;
@@ -41,151 +55,165 @@ import static java.util.Optional.ofNullable;
  */
 class SpringParser implements PathParser {
 
-    private static final Logger LOG = LoggerFactory.getLogger(SpringParser.class);
+	private static final Logger LOG = LoggerFactory.getLogger(SpringParser.class);
 
-    private final PropertyUtils propertyUtils;
-    private final AnnotationUtils annotationUtils;
+	private final PropertyUtils propertyUtils;
+	private final AnnotationUtils annotationUtils;
 
-    SpringParser(PropertyUtils propertyUtils, AnnotationUtils annotationUtils) {
-        requireNonNull(propertyUtils);
-        requireNonNull(annotationUtils);
-        this.propertyUtils = propertyUtils;
-        this.annotationUtils = annotationUtils;
-    }
+	SpringParser(PropertyUtils propertyUtils, AnnotationUtils annotationUtils) {
+		requireNonNull(propertyUtils);
+		requireNonNull(annotationUtils);
+		this.propertyUtils = propertyUtils;
+		this.annotationUtils = annotationUtils;
+	}
 
-    @Override
-    public List<ParsedPath> parse(Element annotatedElement) {
-        if (annotatedElement.getKind() != ElementKind.CLASS) {
-            return emptyList();
-        }
-        if (annotatedElement.getModifiers().contains(Modifier.ABSTRACT)) {
-            return emptyList();
-        }
+	@Override
+	public List<ParsedPath> parse(Element annotatedElement) {
+		if (annotatedElement.getKind() != ElementKind.CLASS) {
+			return emptyList();
+		}
+		if (annotatedElement.getModifiers().contains(Modifier.ABSTRACT)) {
+			return emptyList();
+		}
 
-        Map<String, List<ExecutableElement>> methodsByPathTemplate = new HashMap<>();
+		Map<String, List<ExecutableElement>> methodsByPathTemplate = new HashMap<>();
 
-        annotatedElement
-                .getEnclosedElements()
-                .stream()
-                .filter(o -> o.getKind() == ElementKind.METHOD)
-                .filter(o -> nonNull(o.getAnnotation(RequestMapping.class)))
-                .map(ExecutableElement.class::cast)
-                .forEach(
-                        executableElement -> getPathTemplates(executableElement.getAnnotation(RequestMapping.class))
-                                .stream()
-                                .peek(pathTemplate -> LOG.debug("Extracted path template '{}'", pathTemplate))
-                                .forEach(pathTemplate -> {
-                                    methodsByPathTemplate.computeIfAbsent(pathTemplate, pt -> new ArrayList<>());
-                                    methodsByPathTemplate.get(pathTemplate).add(executableElement);
-                                })
-                );
+		annotatedElement
+				.getEnclosedElements()
+				.stream()
+				.filter(o -> o.getKind() == ElementKind.METHOD)
+				.filter(o -> nonNull(o.getAnnotation(RequestMapping.class)))
+				.map(ExecutableElement.class::cast)
+				.forEach(
+						executableElement -> getPathTemplates(executableElement.getAnnotation(RequestMapping.class))
+								.stream()
+								.peek(pathTemplate -> LOG.debug("Extracted path template '{}'", pathTemplate))
+								.forEach(pathTemplate -> {
+									methodsByPathTemplate.computeIfAbsent(pathTemplate, pt -> new ArrayList<>());
+									methodsByPathTemplate.get(pathTemplate).add(executableElement);
+								})
+				);
 
-        LOG.debug("Found methods by template {}", methodsByPathTemplate);
+		LOG.debug("Found methods by template {}", methodsByPathTemplate);
 
-        Set<String> basePathTemplates = getPathTemplates(annotatedElement.getAnnotation(RequestMapping.class));
-        LOG.debug("Found base path templates {}", basePathTemplates);
-        List<ParsedPath> parsedPaths = new ArrayList<>();
-        methodsByPathTemplate
-                .forEach((pathTemplate, executableElements) -> {
-                    Path path = buildPath(executableElements);
-                    basePathTemplates.forEach(basePathTemplate -> parsedPaths.add(new ParsedPath(basePathTemplate + pathTemplate, path)));
-                });
-        return parsedPaths;
-    }
+		Set<String> basePathTemplates = getPathTemplates(annotatedElement.getAnnotation(RequestMapping.class));
+		LOG.debug("Found base path templates {}", basePathTemplates);
+		List<ParsedPath> parsedPaths = new ArrayList<>();
+		methodsByPathTemplate
+				.forEach((pathTemplate, executableElements) -> {
+					Path path = buildPath(executableElements);
+					basePathTemplates.forEach(basePathTemplate -> parsedPaths.add(new ParsedPath(basePathTemplate + pathTemplate, path)));
+				});
+		return parsedPaths;
+	}
 
-    private Path buildPath(List<ExecutableElement> executableElements) {
-        Path path = new Path();
-        executableElements
-                .stream()
-                .peek(executableElement -> LOG.debug("Building path for {}", executableElement))
-                .forEach(executableElement -> {
-                    Operation operation = buildOperation(executableElement);
-                    RequestMapping requestMapping = executableElement.getAnnotation(RequestMapping.class);
-                    addOperation(path, operation, requestMapping);
-                });
+	private Path buildPath(List<ExecutableElement> executableElements) {
+		Path path = new Path();
+		executableElements
+				.stream()
+				.peek(executableElement -> LOG.debug("Building path for {}", executableElement))
+				.forEach(executableElement -> {
+					Operation operation = buildOperation(executableElement);
+					RequestMapping requestMapping = executableElement.getAnnotation(RequestMapping.class);
+					addOperation(path, operation, requestMapping);
+				});
 
-        return path;
-    }
+		return path;
+	}
 
-    private void addOperation(Path path, Operation operation, RequestMapping requestMapping) {
-        RequestMethod[] methods = requestMapping.method();
-        if (methods.length == 0) {
-            methods = new RequestMethod[]{RequestMethod.GET};
-        }
-        Stream.of(methods)
-                .forEach(requestMethod -> path.set(requestMethod.name().toLowerCase(), operation));
-    }
+	private void addOperation(Path path, Operation operation, RequestMapping requestMapping) {
+		RequestMethod[] methods = requestMapping.method();
+		if (methods.length == 0) {
+			methods = new RequestMethod[]{RequestMethod.GET};
+		}
+		Stream.of(methods)
+				.forEach(requestMethod -> path.set(requestMethod.name().toLowerCase(), operation));
+	}
 
-    private Operation buildOperation(ExecutableElement executableElement) {
-        Operation operation = new Operation();
-        String operationId = ofNullable(executableElement.getAnnotation(ApiOperation.class))
-                .map(ApiOperation::nickname)
-                .filter(StringUtils::isNotBlank)
-                .orElse(executableElement.getSimpleName().toString());
-        operation.setOperationId(operationId);
+	private Operation buildOperation(ExecutableElement executableElement) {
+		Operation operation = new Operation();
+		String operationId = ofNullable(executableElement.getAnnotation(ApiOperation.class))
+				.map(ApiOperation::nickname)
+				.filter(StringUtils::isNotBlank)
+				.orElse(executableElement.getSimpleName().toString());
+		operation.setOperationId(operationId);
 
-        Element controller = executableElement.getEnclosingElement();
-        Optional<Api> apiAnnotation = ofNullable(controller.getAnnotation(Api.class));
-        apiAnnotation.ifPresent(annotation -> Stream.of(annotation.tags())
-                .filter(StringUtils::isNotBlank)
-                .forEach(operation::addTag));
+		Element controller = executableElement.getEnclosingElement();
+		Optional<Api> apiAnnotation = ofNullable(controller.getAnnotation(Api.class));
+		apiAnnotation.ifPresent(annotation -> Stream.of(annotation.tags())
+				.filter(StringUtils::isNotBlank)
+				.forEach(operation::addTag));
 
-        executableElement.getParameters()
-                .stream()
-                .map(this::buildParameter)
-                .filter(Optional::isPresent)
-                .map(Optional::get)
-                .forEach(operation::addParameter);
+		executableElement.getParameters()
+				.stream()
+				.map(this::buildParameter)
+				.filter(Optional::isPresent)
+				.map(Optional::get)
+				.forEach(operation::addParameter);
 
-        Response okResponse = new Response();
-        Property returnProperty = ofNullable(executableElement.getAnnotation(ApiOperation.class))
-                .map(apiOperation -> annotationUtils.extractType(apiOperation, ApiOperation::response))
-                .filter(returnType -> returnType.getKind() != TypeKind.VOID)
-                .map(propertyUtils::toProperty)
-                .orElseGet(() -> propertyUtils.toProperty(executableElement.getReturnType()));
-        okResponse.schema(returnProperty);
-        operation.response(200, okResponse);
+		Response okResponse = new Response();
+		Property returnProperty = ofNullable(executableElement.getAnnotation(ApiOperation.class))
+				.map(apiOperation -> annotationUtils.extractType(apiOperation, ApiOperation::response))
+				.filter(returnType -> returnType.getKind() != TypeKind.VOID)
+				.map(propertyUtils::toProperty)
+				.orElseGet(() -> propertyUtils.toProperty(executableElement.getReturnType()));
+		okResponse.schema(returnProperty);
+		operation.response(200, okResponse);
 
-        return operation;
-    }
+		return operation;
+	}
 
-    private Optional<Parameter> buildParameter(VariableElement variableElement) {
-        if (variableElement.getAnnotation(RequestBody.class) != null) {
-            return Optional.of(buildBodyParameter(variableElement));
-        }
-        PathVariable pathVariable = variableElement.getAnnotation(PathVariable.class);
-        if (pathVariable != null) {
-            return Optional.of(buildPathParameter(variableElement, pathVariable));
-        }
-        return Optional.empty();
-    }
+	private Optional<Parameter> buildParameter(VariableElement variableElement) {
+		if (variableElement.getAnnotation(RequestBody.class) != null) {
+			return Optional.of(buildBodyParameter(variableElement));
+		}
+		PathVariable pathVariable = variableElement.getAnnotation(PathVariable.class);
+		if (pathVariable != null) {
+			return Optional.of(buildPathParameter(variableElement, pathVariable));
+		}
+		RequestParam requestParam = variableElement.getAnnotation(RequestParam.class);
+		if (requestParam != null) {
+			return Optional.of(buildQueryParameter(variableElement, requestParam));
+		}
+		return Optional.empty();
+	}
 
-    private BodyParameter buildBodyParameter(VariableElement variableElement) {
-        BodyParameter bodyParameter = new BodyParameter();
-        bodyParameter.setName(variableElement.getSimpleName().toString());
-        bodyParameter.setRequired(true);
-        bodyParameter.setSchema(new ModelImpl().type(ModelImpl.OBJECT));
-        return bodyParameter;
-    }
+	private BodyParameter buildBodyParameter(VariableElement variableElement) {
+		BodyParameter bodyParameter = new BodyParameter();
+		bodyParameter.setName(variableElement.getSimpleName().toString());
+		bodyParameter.setRequired(true);
+		bodyParameter.setSchema(new ModelImpl().type(ModelImpl.OBJECT));
+		return bodyParameter;
+	}
 
-    private PathParameter buildPathParameter(VariableElement variableElement, PathVariable pathVariable) {
-        Property property = ofNullable(propertyUtils.toSimpleProperty(variableElement))
-                .filter(prop -> !(prop instanceof ObjectProperty))
-                .orElseGet(StringProperty::new);
-        return new PathParameter()
-                .name(pathVariable.value())
-                .required(pathVariable.required())
-                .property(property);
-    }
+	private QueryParameter buildQueryParameter(VariableElement variableElement, RequestParam requestParam) {
+		Property property = ofNullable(propertyUtils.toSimpleProperty(variableElement))
+				.filter(prop -> !(prop instanceof ObjectProperty))
+				.orElseGet(StringProperty::new);
+		return new QueryParameter()
+				.name(requestParam.value())
+				.required(requestParam.required())
+				.property(property);
+	}
 
-    private Set<String> getPathTemplates(RequestMapping requestMapping) {
-        if (requestMapping.value().length > 0) {
-            return new HashSet<>(Arrays.asList(requestMapping.value()));
-        } else if (requestMapping.path().length > 0) {
-            return new HashSet<>(Arrays.asList(requestMapping.path()));
-        } else {
-            return Collections.singleton(StringUtils.EMPTY);
-        }
-    }
+	private PathParameter buildPathParameter(VariableElement variableElement, PathVariable pathVariable) {
+		Property property = ofNullable(propertyUtils.toSimpleProperty(variableElement))
+				.filter(prop -> !(prop instanceof ObjectProperty))
+				.orElseGet(StringProperty::new);
+		return new PathParameter()
+				.name(pathVariable.value())
+				.required(pathVariable.required())
+				.property(property);
+	}
+
+	private Set<String> getPathTemplates(RequestMapping requestMapping) {
+		if (requestMapping.value().length > 0) {
+			return new HashSet<>(Arrays.asList(requestMapping.value()));
+		} else if (requestMapping.path().length > 0) {
+			return new HashSet<>(Arrays.asList(requestMapping.path()));
+		} else {
+			return Collections.singleton(StringUtils.EMPTY);
+		}
+	}
 
 }
--- src/test/resources/standard/spring/specification_only/input/Controller.java
+++ src/test/resources/standard/spring/specification_only/input/Controller.java
@@ -1,16 +1,16 @@
 package standard.spring.empty.input;
 
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Api(tags = "foo")
 @RequestMapping("/foo")
 public class Controller {
 
-	@RequestMapping(value = "/hello/{name}", method = RequestMethod.GET)
-	public Object hello(@PathVariable(value = "name", required = false) String name) {
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public Object hello(@RequestParam(value = "name", required = false) String name) {
 		return "Hello " + name;
 	}
 
